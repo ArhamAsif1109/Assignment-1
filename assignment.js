754dbb359ef0eb1229142e129182443ac63aa7f9
ArhamAsif1109 <168767144+ArhamAsif1109@users.noreply.github.com>
2024-05-10 11:04:11 +0500

    Add files via upload
    initial commit

---
+++ assignment.js
@@ -0,0 +1,19 @@
+"use strict";
+question;
+no;
+1;
+console.log("hello world");
+question;
+no;
+2;
+let temperature = 10;
+if (temperature < 20) {
+    console.log("it's a cold day");
+}
+question;
+no;
+3;
+let apples = 10;
+let givenaway = 3;
+let remainingapples = apples - givenaway;
+console.log("remainingapples");
---
+++ index.ts
@@ -0,0 +1,19 @@
+console.log("hello i am from panaverse");
+ console.log("hello world");
+ console.log("my name is Arham ASIF");
+
+
+
+ console.log("i live in pakistan");
+
+
+let firstname3;
+let firstname:string="arham";
+let temperature=10;
+if (temperature<20)
+console.log("it's a cold");
+else
+console.log("it's not a cold");
+
+
+
